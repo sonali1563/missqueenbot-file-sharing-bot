b1e4dccc025c61dc0b61654b2b154024f5a92245
Sonali Saha <152001935+sonali1563@users.noreply.github.com>
2024-02-28 18:28:24 +0530

    files uploaded✅

---
+++ CONTRIBUTING.md
@@ -0,0 +1,19 @@
+# Contributing to FILE-SHARING-BOT
+We love your input! We want to make contributing to this project as easy and transparent as possible, whether it's:
+
+- Reporting a bug
+- Discussing the current state of the code
+- Submitting a fix
+- Proposing new features
+
+## We Develop with Github
+We use github to host code, to track issues and feature requests, as well as accept pull requests.
+
+1. Fork the repo and create your branch from `MAIN`.
+2. If you've added code, please test it.
+3. Make sure your code works.
+4. Issue that pull request! (develop branch)
+
+## Any contributions you make will be under the GNU General Public License v3.0
+In short, when you submit code changes, your submissions are understood to be under the same [GNU General Public License v3.0] that covers the project. Feel free to contact the maintainers if that's a concern.
+
---
+++ config.py
@@ -0,0 +1,85 @@
+#(©)CodeXBotz
+
+
+
+
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+
+#Bot token @Botfather
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6340465184:AAGcALrZpY82tmJAXsI6EKscst7lyIPOvh4")
+
+#Your API ID from my.telegram.org
+APP_ID = int(os.environ.get("APP_ID", "21240172"))
+
+#Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "71e4d726e727190eaa8c0486f0d7e3d6")
+
+#Your db channel Id
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001960936264"))
+
+#OWNER ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "5104903730"))
+
+#Port
+PORT = os.environ.get("PORT", "8080")
+
+#Database 
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://url1564:url1564@cluster0.zko09ty.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp")
+DB_NAME = os.environ.get("DATABASE_NAME", "url1564")
+
+#force sub channel id, if you want enable force sub
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001986933012"))
+
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+
+#start message
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
+try:
+    ADMINS=[]
+    for x in (os.environ.get("ADMINS", "5104903730").split()):
+        ADMINS.append(int(x))
+except ValueError:
+        raise Exception("Your Admins list does not contain valid integers.")
+
+#Force sub message 
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+
+#set your Custom Caption here, Keep None for Disable Custom Caption
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
+
+#set True if you want to prevent users from forwarding files from bot
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
+
+#Set true if you want Disable your Channel Posts Share button
+DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
+
+BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
+USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+
+ADMINS.append(OWNER_ID)
+ADMINS.append(1250450587)
+
+LOG_FILE_NAME = "filesharingbot.txt"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
